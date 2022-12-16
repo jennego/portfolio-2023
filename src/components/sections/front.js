570a6ea8196b782ab6aecda4d9bbf20b43098c8d
Jennifer Chow <jennego@gmail.com>
2022-12-15 22:37:24 -0800

    add font image and overlay

--- src/components/sections/front.js
+++ src/components/sections/front.js
@@ -1,12 +1,23 @@
 import * as React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 
 const FrontPage = () => {
   return (
-    <div className="title-screen">
-      <div class="title-text-group">
-        <h1 className="title">Jennifer Chow</h1>
+    <>
+      <div className="image-container">
+        <StaticImage
+          src="../../images/ray.jpg"
+          objectFit="cover"
+          layout="fullWidth"
+          className="gatsby-ray"
+        />
       </div>
-    </div>
+      <div className="title-screen">
+        <div class="title-text-group">
+          <h1 className="title">Jennifer Chow</h1>
+        </div>
+      </div>
+    </>
   )
 }
 
