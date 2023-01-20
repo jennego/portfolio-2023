95227aaf17a4138e45045cfc7306a1d7e00324ca
Jennifer Chow <jennego@gmail.com>
2023-01-19 23:00:05 -0800

    beginings of the side nav

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -3,18 +3,21 @@ import ScrollSpy from "react-scrollspy-navigation"
 
 const SideNav = () => {
   return (
-    <div className="side-nav">
-      <ScrollSpy>
-        <a href="#home" ref={React.createRef()}>
-          Home
-        </a>
-        <a href="#portfolio" ref={React.createRef()}>
-          Portfolio
-        </a>
-        <a href="#about" ref={React.createRef()}>
-          About
-        </a>
-      </ScrollSpy>
+    <div className="side-nav-container">
+      <div className="side-nav">
+        <ScrollSpy>
+          Just use bullet nav!!
+          <a href="#home" ref={React.createRef()}>
+            Home
+          </a>
+          <a href="#portfolio" ref={React.createRef()}>
+            Portfolio
+          </a>
+          <a href="#about" ref={React.createRef()}>
+            About
+          </a>
+        </ScrollSpy>
+      </div>
     </div>
   )
 }
