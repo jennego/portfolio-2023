74db1a0d34e8c9bf2753a52a7de65460ed06da59
Jennifer Chow <jennego@gmail.com>
2023-01-27 23:43:08 -0800

    add about, adjust font size

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { GatsbyImage } from "gatsby-plugin-image"
 import { useStaticQuery, graphql } from "gatsby"
+import { renderRichText } from "gatsby-source-contentful/rich-text"
 
 const About = () => {
   const data = useStaticQuery(graphql`
@@ -8,18 +9,21 @@ const About = () => {
       allContentfulSiteInfo {
         edges {
           node {
-            image {
-              gatsbyImageData(height: 300, width: 400, aspectRatio: 1.5)
+            aboutRt {
+              raw
             }
           }
         }
       }
     }
   `)
+
+  const text = data.allContentfulSiteInfo.edges[0].node.aboutRt
   return (
     <div id="about">
       <h2>About</h2>
       <p> Front-end Dev. Horse lover.Equestrian. Weirdo.</p>
+      {renderRichText(text)}
     </div>
   )
 }
