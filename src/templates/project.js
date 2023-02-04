87c662f1b55eed760950ec0c5f0d0db96316ecc0
Jennifer Chow <jennego@gmail.com>
2023-02-03 22:35:44 -0800

    updated a bunch of packages, start to prep project page

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,5 +1,8 @@
 import * as React from "react"
 import { graphql } from "gatsby"
+import "flexboxgrid/dist/flexboxgrid.min.css"
+import { GatsbyImage } from "gatsby-plugin-image"
+import { renderRichText } from "gatsby-source-contentful/rich-text"
 
 export const query = graphql`
   query projectQuery($id: String!) {
@@ -33,6 +36,15 @@ const Project = ({ data, pageContext }) => {
   return (
     <div>
       <h1>{project.name}</h1>
+      <div className="row">
+        <div className="col-sm-12 col-md 8">
+          <GatsbyImage image={project.mainPhoto.gatsbyImageData} />
+        </div>
+        <div className="col-sm-12 col-lg-4">Bunch of info</div>
+      </div>
+      <div className="row">
+        <div className="col-12"></div>
+      </div>
       {console.log(pageContext.id)}
     </div>
   )
