759b946a6523eaf4891f602579f7c217d8d499dc
Jennifer Chow <jennego@gmail.com>
2023-02-08 23:49:57 -0800

    I dunno did some things but not very much

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,7 +1,26 @@
 import * as React from "react"
 
 const Categories = () => {
-  return <div>Filter by:</div>
+  return (
+    <div>
+      Filter by: Type
+      <ul>
+        <li> Single Page Application (SPA) </li>
+        <li>Website/Blog</li>
+        <li> Web App</li>
+        <li> Other </li>
+      </ul>
+      Technology:
+      <ul>
+        <li>React</li>
+        <li>Gatsby</li>
+        <li>WordPress </li>
+        <li>Ruby on Rails</li>
+        <li> Fauna</li>
+        <li> External Apis </li>
+      </ul>
+    </div>
+  )
 }
 
 export default Categories
