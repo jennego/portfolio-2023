c17a44ab9697d0640e7cd1a4252d3b7d64ac17b8
Jennifer Chow <jennego@gmail.com>
2023-02-13 21:47:46 -0800

    nothing much

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,7 +8,7 @@ const NotFoundPage = () => (
   <Layout>
     <Nav />
     <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>Page doesn't exist</p>
   </Layout>
 )
 
