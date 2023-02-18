4394b2515a2e5676f191a576198d386c363fa980
Jennifer Chow <jennego@gmail.com>
2023-02-17 23:45:10 -0800

    jot stuff in contact

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -1,5 +1,12 @@
 const Contact = () => {
-  return <div>Contact</div>
+  return (
+    <div>
+      <h2>Find Me</h2>
+      <div>(Not in a creepy way, please.)</div>
+      My other sites: Jennego a collection of other, non-web dev things like
+      photography Pixel Ponies mostly cozy and equestrian gaming on Steam Deck
+    </div>
+  )
 }
 
 export default Contact
