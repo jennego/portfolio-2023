b5c720a491059d32ae56de2ecd86d6d5af871896
Jennifer Chow <jennego@gmail.com>
2023-10-04 17:52:58 -0700

    add link to arrow

--- src/components/sections/front.js
+++ src/components/sections/front.js
@@ -2,34 +2,48 @@ import * as React from "react"
 import { StaticImage } from "gatsby-plugin-image"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons"
+import ScrollToPlugin from "gsap/ScrollToPlugin"
+import { gsap } from "gsap"
+import ScrollSpy from "react-scrollspy-navigation"
 
 const FrontPage = () => {
+  const handleClick = e => {
+    gsap.registerPlugin(ScrollToPlugin)
+    gsap.to(window, { duration: 2, scrollTo: 900 })
+
+    console.log("i am going to scroll")
+  }
+
   return (
     <div className="section">
-
-    <div id="home">
-      <div className="image-container">
-        <StaticImage
-          src="../../images/ray.jpg"
-          objectFit="cover"
-          layout="fullWidth"
-          className="gatsby-ray"
-          alt="Horse"
+      <div id="home">
+        <div className="image-container">
+          <StaticImage
+            src="../../images/ray.jpg"
+            objectFit="cover"
+            layout="fullWidth"
+            className="gatsby-ray"
+            alt="Horse"
           />
-      </div>
-      <div className="title-screen">
-        <div className="title-text-group">
-          <h1 className="title">Jennifer Chow</h1>
         </div>
-        <br />
-        <p>Web Developer</p>
+        <div className="title-screen">
+          <div className="title-text-group">
+            <h1 className="title">Jennifer Chow</h1>
+          </div>
+          <p>Web Developer</p>
 
-        <div style={{ paddingTop: "2rem" }}>
-          <FontAwesomeIcon icon={faArrowDown} size="2x" />
+          <div style={{ paddingTop: "1rem" }}>
+            <a
+              onClick={handleClick}
+              ref={React.createRef()}
+              className="clickable-icon"
+            >
+              <FontAwesomeIcon icon={faArrowDown} size="2x" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
-          </div>
   )
 }
 
